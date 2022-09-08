1e3f0b2c665a4765aa991ec24e6953880499b6b3
cedricn8 <cedcn-9mail@yahoo.com>
2022-09-08 14:39:07 -0700

    commit tree f3a434521fc2322610b411001b162a734b1983c8

---
+++ lib/dog.rb
@@ -0,0 +1,30 @@
+class Dog
+
+    #setter method
+    def name=(name)
+
+        @name = name
+    
+    end
+
+    #getter method
+    def name
+
+        @name
+
+    end
+
+    #setter method
+    def breed=(breed)
+
+        @breed = breed
+
+    end
+
+    def breed
+
+        @breed
+
+    end
+
+end
